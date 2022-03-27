46aae0ec57e00ae1caab72360eed519f69ce550f
Dylan Jung <Dj5822@gmail.com>
2022-03-27 23:29:31 +1300

    Add mongoolia plugin to person model

--- backend/src/models/person.model.ts
+++ backend/src/models/person.model.ts
@@ -1,7 +1,13 @@
+/* eslint-disable linebreak-style */
 /**
  * Model defines a datatype's schema (kinda like class)
  */
 import mongoose, { Schema, model } from 'mongoose';
+import mongoolia from 'mongoolia';
+
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 export interface PersonModel {
   first_name: string,
@@ -20,19 +26,27 @@ export interface PersonModel {
 }
 
 const schema = new Schema<PersonModel>({
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: false },
+  first_name: { type: String, required: true, algoliaIndex: true },
+  last_name: { type: String, required: false, algoliaIndex: true },
   birthday: { type: Date, required: false },
-  gender: { type: String, required: false },
-  location: { type: String, required: false },
+  gender: { type: String, required: false, algoliaIndex: true },
+  location: { type: String, required: false, algoliaIndex: true },
   first_met: { type: Date, required: false },
-  how_we_met: { type: String, required: false },
-  interests: { type: [String], required: false },
-  organisation: { type: String, required: false },
-  social_media: { type: Map, of: String, required: false },
+  how_we_met: { type: String, required: false, algoliaIndex: true },
+  interests: { type: [String], required: false, algoliaIndex: true },
+  organisation: { type: String, required: false, algoliaIndex: true },
+  social_media: {
+    type: Map, of: String, required: false, algoliaIndex: true,
+  },
   image: { type: Buffer, required: false },
   encounters: { type: [mongoose.Types.ObjectId], required: false },
   time_updated: { type: Date, default: new Date(Date.now()), required: true },
+});
+
+schema.plugin(mongoolia, {
+  appId: `${process.env.ALGOLIA_APP_ID}`,
+  apiKey: `${process.env.ALGOLIA_SECRET_KEY}`,
+  indexName: 'persons',
 });
 
 export default model<PersonModel>('Person', schema);
